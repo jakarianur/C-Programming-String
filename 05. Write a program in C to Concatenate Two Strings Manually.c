597dd12819e341422b9c92a2c53994deca97b233
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-16 18:39:38 +0600

    Update 05. Write a program in C to Concatenate Two Strings Manually.c

--- 05. Write a program in C to Concatenate Two Strings Manually.c
+++ 05. Write a program in C to Concatenate Two Strings Manually.c
@@ -1,1 +1,26 @@
 
+#include <stdio.h>
+int main()
+{
+char str1[100], str2[100],str3[100];
+int i,j;
+printf("Enter your first string: ");
+fgets (str1, sizeof(str1), stdin);
+printf("Enter your second string: ");
+fgets (str2, sizeof(str2), stdin);
+
+while (str1[i] != '\0') {
+		str3[j] = str1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (str2[i] != '\0') {
+		str3[j] = str2[i];
+		i++;
+		j++;
+	}
+	str3[j] = '\0';
+	printf("Concatenated String: %s", str3);
+	return 0;
+}
