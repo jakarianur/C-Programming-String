6022ff195ce12ed5305387314183cdf89fe0446b
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-16 14:41:52 +0600

    Update 3. Write a program in C to print individual characters of string in reverse order.c

--- 3. Write a program in C to print individual characters of string in reverse order.c
+++ 3. Write a program in C to print individual characters of string in reverse order.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main()
+{
+char str[100];
+int length = 0;
+printf("\nEnter a string: ");
+scanf( "%s",str);
+while (str [length] != '\0')
+{
+    length++ ;
+}
+printf("\n Individual character for this string Reverse: \n");
+for (int i = length -1; i>=0; i--)
+{
+  printf (" %c\n", str[i]);
+}
+return 0;
+}
