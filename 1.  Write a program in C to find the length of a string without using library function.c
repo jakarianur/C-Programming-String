6562101f061878d37275bc5219727b035eaa1e51
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-16 14:25:51 +0600

    Create 1.  Write a program in C to find the length of a string without using library function.c

---
+++ 1.  Write a program in C to find the length of a string without using library function.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+char str[100];
+int length = 0;
+printf("\nEnter a string: ");
+scanf( "%s",str);
+while (str [length] != '\0'){
+    length++ ;
+}
+printf ("\nLength of this string: %d \n", length);
+return 0;
+}
