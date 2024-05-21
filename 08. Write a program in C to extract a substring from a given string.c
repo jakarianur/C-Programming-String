012b8e4fae6b13a97772d5be8d91c7da83d7672e
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-21 20:08:34 +0600

    Update 08. Write a program in C to extract a substring from a given string.c

--- 08. Write a program in C to extract a substring from a given string.c
+++ 08. Write a program in C to extract a substring from a given string.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main()
+{
+   int pos, len, c;
+   char str[100],subs[100];
+   printf("Enter a string: \n");
+   fgets (str, sizeof(str), stdin);
+   printf("Enter position of the extracted string: \n");
+   scanf("%d", &pos);
+   printf("Enter length of the extracted string: \n");
+   scanf("%d", &len);
+   c=0;
+   while (c<len)
+   {
+       subs[c]=str[pos+c];
+       c++;
+   }
+       subs[c]='\0';
+       printf("\nThe string after extraction: %s\n", subs);
+    return 0;
+}
