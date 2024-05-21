55cdab3bfabb914108bdfcac48e93bfaad71a7e8
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-21 20:29:53 +0600

    Update 07. Write a program in C to Find the Frequency of Characters.c

--- 07. Write a program in C to Find the Frequency of Characters.c
+++ 07. Write a program in C to Find the Frequency of Characters.c
@@ -1,1 +1,21 @@
-7. Write a program in C to Find the Frequency of Characters.
+#include <stdio.h>
+int main()
+{
+    char str[50];
+    int frequency[256] = {0};
+    printf("Enter a string: \n");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        frequency[(unsigned char)str[i]]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (frequency[i] > 0 && i != ' ' && i != '\t' && i != '\n')
+        {
+            printf("%c has occurred %d times\n", i, frequency[i]);
+        }
+    }
+    return 0;
+}
