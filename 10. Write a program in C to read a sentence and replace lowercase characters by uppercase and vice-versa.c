ddb98dea5179b9c8886742dbe8d2b2cf53b1da48
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 15:50:18 +0600

    Update 10. Write a program in C to read a sentence and replace lowercase characters by uppercase and vice-versa.c

--- 10. Write a program in C to read a sentence and replace lowercase characters by uppercase and vice-versa.c
+++ 10. Write a program in C to read a sentence and replace lowercase characters by uppercase and vice-versa.c
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main()
+{
+    int i;
+    char str[100];
+    printf("Enter your first string: \n");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; str[i] != '\0'; i++)
+    {
+    if(str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i]=str[i] + 32;
+        }
+    else if (str[i] >= 'a' && str[i] <= 'z')
+    {
+            str[i]=str[i] - 32;
+    }
+    }
+            printf("After replacing, this string is: %s\n", str);
+    return 0;
+}
