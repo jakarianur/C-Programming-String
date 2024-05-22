3d3f66c55f4c3b64e8b8d575469cf88e5ff2daf4
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 16:34:26 +0600

    Update 12. Write a program in C to compare two strings without using string library functions.c

--- 12. Write a program in C to compare two strings without using string library functions.c
+++ 12. Write a program in C to compare two strings without using string library functions.c
@@ -1,1 +1,27 @@
-
+#include <stdio.h>
+int main()
+{
+    int i;
+    char str1[100], str2[100];
+    printf("Enter string 1: \n");
+    fgets(str1, sizeof(str1), stdin);
+     printf("Enter string 2: \n");
+    fgets(str2, sizeof(str2), stdin);
+    while(str1[i]==str2[i] && str1[i]!='\0')
+    {
+        i++;
+    }
+    if (str1[i] > str2[i])
+    {
+        printf("\nString 1 > Srting 2\n");
+    }
+    else if (str1[i] < str2[i])
+    {
+        printf("\nString 1 < Srting 2\n");
+    }
+    else
+    {
+        printf("\nString 1 = Srting 2\n");
+    }
+    return 0;
+}
