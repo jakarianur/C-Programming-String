aca11733c7ca7805629ee98707df4c554f0c213d
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 15:34:25 +0600

    Update 09. Write a C program to copy  one string to another string without using library function.c

--- 09. Write a C program to copy  one string to another string without using library function.c
+++ 09. Write a C program to copy  one string to another string without using library function.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+int main()
+{
+    char str1[100], str2[100] ;
+    int i;
+    printf("Enter your first string: \n");
+    fgets(str1, sizeof(str1), stdin);
+     printf("Enter your second string: \n");
+    fgets(str2, sizeof(str2), stdin);
+    for (i = 0; str1[i] != '\0'; i++)
+    {
+        str2[i]=str1[i];
+    }
+    str2[i]='\0';
+            printf("After copying, this string is: %s\n", str2);
+    return 0;
+}
