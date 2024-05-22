32662fd10e8769e06b6e5489b1efcbac2364d5cc
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-22 16:13:12 +0600

    Update 11. Write a program in C to find maximum occurring character in a string.c

--- 11. Write a program in C to find maximum occurring character in a string.c
+++ 11. Write a program in C to find maximum occurring character in a string.c
@@ -1,1 +1,28 @@
-
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i, length, max = -1;
+    char str[100], result;
+    int used[256] = {0};
+    printf("Enter a string: \n");
+    fgets(str, sizeof(str), stdin);
+    
+    str[strcspn(str, "\n")] = '\0';
+    length = strlen(str);
+    
+    for (i = 0; i < length; i++)
+    {
+        used[(unsigned char)str[i]]++;
+    }
+    for (i = 0; i < 256; i++)
+    {
+        if (used[i] > max)
+        {
+            max = used[i];
+            result = (char)i;
+        }
+    }
+    printf("The maximum occurring character of the given string is: %c\n", result);
+    return 0;
+}
