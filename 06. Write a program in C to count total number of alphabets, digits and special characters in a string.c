37ca9b86edc57c0a52521bbd56c651f6b3f15456
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:27:54 +0600

    Update 06. Write a program in C to count total number of alphabets, digits and special characters in a string.c

--- 06. Write a program in C to count total number of alphabets, digits and special characters in a string.c
+++ 06. Write a program in C to count total number of alphabets, digits and special characters in a string.c
@@ -8,30 +8,35 @@ printf("\nEnter a string: ");
 fgets (str, sizeof(str), stdin);
 for (i=0; str[i] != '\0'; i++)
 {
-    if (isalpha str[i]){
+    if (isalpha str[i])
+    {
         alphabet++;
-        }
-      else if (isdigit str[i]){
-        digit++;
-        }
-        else if (str[i] = ' ' && ){
-        special++;
-        }
     }
-while (str1[i] != '\0') {
+    else if (isdigit str[i])
+    {
+        digit++;
+    }
+    else if (str[i] = ' ' && )
+    {
+        special++;
+    }
+}
+while (str1[i] != '\0')
+	{
 		str3[j] = str1[i];
 		i++;
 		j++;
 	}
-	i = 0;
-	str3[j]= ' ';
-	j++;
-	while (str2[i] != '\0') {
+		i = 0;
+		str3[j]= ' ';
+		j++;
+while (str2[i] != '\0')
+	{
 		str3[j] = str2[i];
 		i++;
 		j++;
 	}
-	str3[j] = '\0';
+		str3[j] = '\0';
 	printf("Concatenated String: %s", str3);
 	return 0;
 }
