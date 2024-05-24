1258423bc4fd401aa59982065f765a678e992daa
Md Jakaria Nur <mdjakarianur.in@gmail.com>
2024-05-24 23:22:53 +0600

    Update 04. Write a program in C to count the total number of words in a string.c

--- 04. Write a program in C to count the total number of words in a string.c
+++ 04. Write a program in C to count the total number of words in a string.c
@@ -7,10 +7,11 @@ printf("\nEnter a string: ");
 fgets (str, sizeof(str), stdin);
 for (i=0; str[i] != '\0'; i++)
 {
-    if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' ){
+    if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+    {
         total++;
-        }
     }
+}
 printf("\n Total Word has this string: %d\n", total);
 return 0;
 }
